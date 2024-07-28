26c1bdb7e3ec26c735a330cf1be0cbf739fd8b19
Leonardo Rossi <leonardrossi1999@gmail.com>
2024-07-28 12:12:35 +0200

    Reduced parameters to the essential

--- main.py
+++ main.py
@@ -44,11 +44,14 @@ def serve_layout():
                 # Text introduction
                 html.Div([
                     dcc.Markdown("""
-                        The simulation is divided in two parts: first, the code generates a certain number of 1D speckle fields, and each field is 
+                        > The simulation is divided in two parts: 
+                                 
+                        > First, the code generates a certain number of 1D speckle fields, and each field is 
                         then spatially filtered to produce a partially coherent field. The number of fields and the type of filtering may be selected 
                         in the boxes below. Since in the physical experiment the speckle field is produced by a rough glass surface which might have 
                         a non-negligible correlation length, it's also possible to generate fields which take that into account.  
-                        In the second part, the code simulates the production of Young interference fringes by passage of the (filtered) field through 
+                                 
+                        > In the second part, the code simulates the production of Young interference fringes by passage of the (filtered) field through 
                         a double slit. It is expected that the visibility of the average pattern (i.e., the correlation length of the filtered field) should 
                         be related to the amount of spatial filtering. 
                     """)
@@ -98,100 +101,63 @@ def serve_layout():
             html.Div(children = [ 
                 # Input parameters for the first part of the simulation
                 html.Div(children = [
-                    html.H3(children = 'Choose the geometry of the set-up and the wavelength'),
+                    html.H3(children = 'Geometry of the set-up and wavelength'),
                     html.Br(),
-                    html.Label(
+                    html.Div(
                         # Input the size of the "source", the source being essentially the portion of the rough glass surface on which the laser shines
-                        dcc.Markdown(r'Size of the source ($\mathrm{cm}$)', mathjax = True) 
+                        dcc.Markdown(r'> We suppose that the beam is initially collimated, with a diameter of $0.5 \, \mathrm{cm}$', mathjax = True) 
                     ),
                     # For now I consider only one transversal and one longitudinal dimension
-                    dcc.Slider(
-                        0.1,
-                        1,
-                        step = 0.05,
-                        value = 0.5,
-                        marks = {str(x): str(x) for x in [0.5, 1]},
-                        id='hor-size'
-                    ),
-                    html.Br(),
-                    # Here the chosen value for the size of the source is shown on screen
-                    html.Div(id = 'hor-size-out'),
+
                     html.Br(),
                     html.Label(
                         # Input the distance over which the field propagates (forming a speckle field in the process) before the filtering
-                        dcc.Markdown(r'Distance from the source to the first image ($\mathrm{cm}$)', mathjax = True)
+                        dcc.Markdown(r'> The speckle field is formed $15 \, \mathrm{cm}$ away from the rough glass surface', mathjax = True)
                     ),
-                    dcc.Slider(
-                        10,
-                        50,
-                        step = 5,
-                        value = 15,
-                        marks = {str(x): str(x) for x in np.arange(10, 60, 10)},
-                        id ='dist'
-                    ),
-                    html.Br(),
-                    # Show chosen value on screen
-                    html.Div(id = 'dist-out'),
+
                     html.Br(),
                     html.Label(
                         # Input laser wavelength
-                        dcc.Markdown(r'Wavelength ($\mathrm{nm}$)', mathjax = True)
-                    ),
-                    dcc.Slider(
-                        400,
-                        600,
-                        step = 5,
-                        value = 500,
-                        marks = {str(x): str(x) for x in np.arange(400, 700, 100)},
-                        id='wave-len'
+                        dcc.Markdown(r'> The wavelength is chosen to be $500 \, \mathrm{nm}$)', mathjax = True)
                     ),
                     html.Br(),
                     # Show chosen value on screeen
-                    html.Div(id = 'wave-len-out'),
                 ],
                 className = 'left'
                 ),
 
                 html.Div(children = [
-                    html.H3(children = 'Select the number of fields to produce, the number of "scatterers" and the correlation length of the rough glass surface'),
+                    html.H3(children = 'Number of fields produced, number of "scatterers" and correlation length of the rough glass surface'),
                     html.Br(),
                     html.Label(
                         # Input the number of speckle fields to produce 
-                        dcc.Markdown('Number of speckle fields to produce')
+                        dcc.Markdown(r"""
+                                     $200$ speckle fields are generated by default for statistics. 
+                                     If the correlation length of the source (i.e. the rough glass surface) is not zero, 
+                                     more statistics could be necessary ($\sim 1000$)
+                                     """, mathjax = True)
                     ),
                     dcc.Slider(
-                        10,
-                        1000,
-                        step = 1,
-                        value = 100,
-                        marks = {str(x): str(x) for x in np.arange(10, 1000, 200)},
+                        200,
+                        2000,
+                        step = 100,
+                        value = 200,
+                        marks = {str(x): str(x) for x in np.arange(200, 2000, 200)},
                         id='field-number'
                     ),
                     html.Br(),
-                    # Show chosen value on screen
                     html.Div(id = 'field-number-out'),
                     html.Br(),
                     html.Label(
                         # Input the number of "scatterers" which produce the field. Essentialy, this number is the number of points of the source.
                         # I found it not practical to randomize the points for the interference part too, but it is possible to do it at least for the 
                         # first part, and this should minimize spurious effects due to the source form and sharp edges. 
-                        dcc.Markdown('Number of scatters per field')
+                        dcc.Markdown(r'The speckle field is generated with a monte carlo randomization, with $1000$ points on the source', mathjax = True)
                     ),
-                    dcc.Slider(
-                        100,
-                        2000,
-                        step = 1,
-                        value = 1000,
-                        marks = {str(x): str(x) for x in np.arange(100, 2000, 400)},
-                        id='scatterer-number'
-                    ),
-                    html.Br(),
-                    # Show chosen value on screen
-                    html.Div(id = 'scatterer-number-out'),
                     html.Br(),
                     html.Label(
                         # Input the correlation length of the surface. 
-                        dcc.Markdown(r'Correlation length of the rough glass surface ($\mu\mathrm{m}$)', mathjax = True)
+                        dcc.Markdown(r'Input the correlation length of the rough glass surface ($\mu\mathrm{m}$)', mathjax = True)
                     ),
                     dcc.Slider(
                         0,
@@ -249,7 +215,7 @@ def serve_layout():
             ),
             html.Div(children = [
                 html.Div(children = [
-                    html.H3(children = 'Select spatial filtering parameters'),
+                    html.H3(children = 'Spatial filtering parameters'),
                     html.Br(),
                     html.Label(
                         # Input the type of filtering, which should give different statistics of the filtered field (i.e., sinc or gaussian correlation function)
@@ -285,27 +251,16 @@ def serve_layout():
                 ),
 
                 html.Div(children = [
-                    html.H3('Choose set-up parameters'),
+                    html.H3('Set-up parameters'),
                     html.Br(),
                     html.Label(
                         # Input the distance from the interference slits and the screen on which the pattern appears. 
-                        dcc.Markdown(r'Distance from slits to screen ($\mathrm{cm}$)', mathjax = True)
+                        dcc.Markdown(r'The interference patterns are calculated in a far field approximation', mathjax = True)
                     ),
-                    dcc.Slider( 
-                        5000,
-                        10000,
-                        step = 50,
-                        value = 7000,
-                        marks = {str(x): str(x) for x in np.arange(5000, 10000, 1000)},
-                        id='dist-2'
-                    ),
-                    html.Br(),
-                    # Show chosen value on screen
-                    html.Div(id = 'dist-2-out'),
                     html.Br(),
                     html.Label(
                         # Input slits spacing
-                        dcc.Markdown(r'Distance between slits ($\mathrm{mm}$)', mathjax = True)
+                        dcc.Markdown(r'Input distance between slits ($\mathrm{mm}$)', mathjax = True)
                     ),
                     dcc.Slider( 
                         4,
@@ -321,19 +276,8 @@ def serve_layout():
                     html.Br(),
                     # Input slit width 
                     html.Label(
-                        dcc.Markdown(r'Slit width ($\mathrm{mm}$)', mathjax = True)
+                        dcc.Markdown(r'The slits are 1 $\mathrm{mm}$ wide', mathjax = True)
                     ),
-                    dcc.Slider( 
-                        1,
-                        4,
-                        step = 0.2,
-                        value = 1,
-                        marks = {str(x): str(x) for x in np.arange(1, 4, 1)},
-                        id='slit-width'
-                    ),
-                    html.Br(),
-                    # Show chosen value on screen
-                    html.Div(id = 'slit-width-out'),
                 ],
                 className = 'right_green'
                 ),
@@ -381,8 +325,7 @@ def serve_layout():
                 html.Div([
                 # Text introduction
                 dcc.Markdown("""
-                    Once generated a bunch of patterns with different filterings with the functions above, refresh the page *only once* before 
-                    starting the analysis.
+                    > Once generated a bunch of patterns with different filterings with the functions above, refresh the page *only once* before starting the analysis.
                 """)
                 ],
                 className = 'box_pink')
@@ -398,10 +341,14 @@ def serve_layout():
             className = 'graph'
             ),
 
+            html.Div(children = [
+                    dcc.Markdown(id = 'patt-param', mathjax = True)
+            ]),
+
             html.Div([
                 html.Div([
                 dcc.Markdown("""
-                    The analysis consists of the calculation of the visibility of the interference patterns, which corresponds to the absolute value of the *field* 
+                    > The analysis consists of the calculation of the visibility of the interference patterns, which corresponds to the absolute value of the *field* 
                     correlation function of the speckle field (absolute value of the *complex degree of coherence*). In addition, the center of the pattern could be a 
                     local maximum, in which case the phase of the correlation function is +1, or a local minimum, in which case the phase is -1.
                 """)
@@ -466,29 +413,17 @@ def render(tab):
         return off, off, off, on
 
 @callback( # This serves to return the values selected in the sliders
-    Output('hor-size-out', 'children'),
-    Output('dist-out', 'children'),
-    Output('wave-len-out', 'children'),
     Output('field-number-out', 'children'),
-    Output('scatterer-number-out', 'children'),
     Output('correlation-length-out', 'children'),
     Output('filter-width-out', 'children'),
-    Output('dist-2-out', 'children'),
     Output('slits-dist-out', 'children'),
-    Output('slit-width-out', 'children'),
-    Input('hor-size', 'value'),
-    Input('dist', 'value'),
-    Input('wave-len', 'value'),
     Input('field-number', 'value'),
-    Input('scatterer-number', 'value'),
     Input('correlation-length', 'value'),
     Input('filter-width', 'value'),
-    Input('dist-2', 'value'),
     Input('slits-dist', 'value'),
-    Input('slit-width', 'value'),
 )
-def update_values(a, b, c, d, e, f, g, h, i, l): 
-    return a, b, c, d, e, f, g, h, i, l
+def update_values(a, b, c, d): 
+    return a, b, c, d
 
 @app.long_callback( 
     # This is the callback for the first simulation. Long callback since for regular callbacks there's a max time of 30 s.
@@ -498,12 +433,9 @@ def update_values(a, b, c, d, e, f, g, h, i, l):
     ],
     inputs = [
         Input('part-one-button', 'n_clicks'), # The only input is the click of the 'start' button
-        State('hor-size', 'value'), # The other parameters are passed as states, so changing them does not trigger the start of the simulation
-        State('dist', 'value'),
+        # The other parameters are passed as states, so changing them does not trigger the start of the simulation,
         State('field-number', 'value'),
-        State('scatterer-number', 'value'),
         State('correlation-length', 'value'),
-        State('wave-len', 'value')
     ],
     running=[ # When the simulation is running,
         (Output('part-one-button', 'disabled'), True, False), # The start button is disabled
@@ -523,13 +455,21 @@ def update_values(a, b, c, d, e, f, g, h, i, l):
     progress = [Output('progress-bar-one', 'value'), Output('progress-bar-one', 'max')], # Link to progress bar id
     manager = long_callback_manager
 )
-def generate_fields(set_progress, n_clicks, source_size, dist, field_num, scatt_num, corr, wavelen):
+def generate_fields(set_progress, n_clicks, field_num, corr):
     if n_clicks is None:
         raise exceptions.PreventUpdate() # This is necessary in order for the simulation not to start automatically upon launching the app
     
+    source_size = 0.5
+    dist = 15
+    scatt_num = 1000
+    wavelen = 500
     for i in range(field_num):
         field, screen = mod.generate_speckle_field(corr, source_size, dist, scatt_num, wavelen) # Generate a field
-        field_data = pd.DataFrame(np.stack((screen, field.real, field.imag), axis = -1), columns = ['screen', 'spec_re', 'spec_im']) # Create a data frame
+        field_data = pd.DataFrame({
+            'screen': screen, 
+            'spec_re': field.real, 
+            'spec_im': field.imag
+        }) # Create a data frame
         field_data.to_csv('Speckles/speckle_num_{}.csv'.format(i)) # Store in csv
         set_progress((str(i + 1), str(field_num))) # Update progress bar
 
@@ -545,10 +485,7 @@ def generate_fields(set_progress, n_clicks, source_size, dist, field_num, scatt_
         Input('part-two-button', 'n_clicks'), # As before, the only input is the click on the button and the other parameters are states.
         State('filtering-type', 'value'),
         State('filter-width', 'value'),
-        State('dist-2', 'value'),
         State('slits-dist', 'value'),
-        State('slit-width', 'value'),
-        State('wave-len', 'value')
     ],
     running=[ # This is identical to above
         (Output('part-two-button', 'disabled'), True, False),
@@ -568,10 +505,13 @@ def generate_fields(set_progress, n_clicks, source_size, dist, field_num, scatt_
     progress=[Output('progress-bar-two', 'value'), Output('progress-bar-two', 'max')],
     manager=long_callback_manager
 )
-def filter_and_interfere(set_progress, n_clicks, filter_type, filter_width, dist_2, slits_dist, slit_width, wavelen):
+def filter_and_interfere(set_progress, n_clicks, filter_type, filter_width, slits_dist):
     if n_clicks is None:
         raise exceptions.PreventUpdate()
-    
+
+    slit_width = 1
+    dist_2 = 1e4
+    wavelen = 500
     screen_size = 20 # [cm] 
     dx = 0.02 # [cm] (resolution)
     dim = int(screen_size/dx) + 1 # Dimension of the arrays
@@ -591,17 +531,23 @@ def filter_and_interfere(set_progress, n_clicks, filter_type, filter_width, dist
         set_progress((str(k), str(len(vect)))) # Update progress bar
         k = k + 1
 
-
-    pattern_data = pd.DataFrame(np.stack((screen, pattern), axis = -1), columns = ['screen', 'pattern']) # Convert to data frame
-    fig = px.line(pattern_data, x = 'screen', y = 'pattern', title = 'Pattern di interferenza mediato') # Create the figure of the graph
-    pattern_data.to_csv('Patterns/Pattern_filt_{}.csv'.format(filter_width)) # Store the pattern to csv
+    pattern_data = pd.DataFrame({
+        'screen': screen,
+        'pattern': pattern,
+        'filter_type': [filter_type for i in range(len(screen))],
+        'filter_width': [filter_width for i in range(len(screen))],
+        'slits_dist': [slits_dist for i in range(len(screen))]
+    }) # Convert to data frame
+    fig = px.line(pattern_data, x = 'screen', y = 'pattern', title = 'Averaged interference pattern') # Create the figure of the graph
+    pattern_data.to_csv('Patterns/Pattern_{}.csv'.format(n_clicks)) # Store the pattern to csv
 
     return ['Simulation number {}'.format(n_clicks + 1)], fig # Return the outputs
 
 @callback(
     # This is the callback for the plot of an individual pattern in the data analysis part. A long callback isn't necessary here
-    Output('pattern-analysis', 'figure'), # Output a counter and the graph
+    Output('pattern-analysis', 'figure'), # Output a counter, the graph and the parameters of the pattern
     Output('counter-plot', 'children'),
+    Output('patt-param', 'children'),
     Input('plot-button', 'n_clicks'), # Input the button click, other parameters are states
     State('select-pattern', 'value')
 )
@@ -610,11 +556,9 @@ def plot_pattern(n_clicks, patt_name):
         raise exceptions.PreventUpdate()
     
     pattern_data = pd.read_csv('Patterns/' + patt_name) # Read the pattern from csv
-    filter_width = patt_name.split('_')[-1].split('.')[0] # Obtain the filter width from the csv file name
-    my_string = r'Interference pattern, filtering = {} '.format(filter_width) 
-    fig = px.line(pattern_data, x = 'screen', y = 'pattern', title = my_string) # Create the figure
+    fig = px.line(pattern_data, x = 'screen', y = 'pattern', title = 'Interference pattern') # Create the figure
 
-    return fig, ['Plot number {}'.format(n_clicks + 1)]
+    return fig, ['Plot number {}'.format(n_clicks + 1)], ['Filter type: ' + pattern_data['filter_type'][0] + ', filter width: {}'.format(pattern_data['filter_width'][0]) + r'$\, \mathrm{cm}^{-1}$' + ', slit separation: {}'.format(pattern_data['slits_dist'][0]) + r'$\, \mathrm{mm}$']
     
 if __name__ == '__main__':
     app.run(debug=True)
